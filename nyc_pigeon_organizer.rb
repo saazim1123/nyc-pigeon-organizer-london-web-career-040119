eb35c34810db619a77a8431a96bd94b73fa79fa9
Adnan Azim <saazim1123@gmail.com>
2019-03-24 14:24:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  
+  data.each do |pigeon, pigeon_hash|
+    pigeon_hash.each do |key, value|
+      value.each do |name|
+        if !hash.has_key?(name)
+          hash[name] = {}
+        end
+        
+        if !hash[name].has_key?(pigeon)
+          hash[name][pigeon] = []
+        end
+        
+        if !hash[name][pigeon].include?(key)
+          hash[name][pigeon] << key.to_s
+        end
+        
+      end
+    end
+  end
+  hash
 end
